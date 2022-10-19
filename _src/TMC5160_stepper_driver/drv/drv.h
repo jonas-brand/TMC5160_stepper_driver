26649566725db2ffd8a84c32c91061c1f2cbe3dd
jonas-brand <jonas.brand@bluewin.ch>
2022-10-19 11:55:29 +0200

    added driver struct

--- _src/TMC5160_stepper_driver/drv/drv.h
+++ _src/TMC5160_stepper_driver/drv/drv.h
@@ -1,4 +1,13 @@
 #ifndef __TMC5160_DRV_H__
 #define __TMC5160_DRV_H__
 
+#include "utils/periferals.h"
+#include "TMC5160_stepper_driver/spi/spi.h"
+
+//struct for stepper driver
+typedef struct
+{
+    drv_idx_t idx;
+}stp_drv_t;
+
 #endif //! __TMC5160_DRV_H__
