e92c86881ef69de587ed7d0aee06b6d8d57d754c
jonas-brand <jonas.brand@bluewin.ch>
2022-10-31 10:17:03 +0100

    added stp_drv_mov function

--- _src/TMC5160_stepper_driver/drv/drv.h
+++ _src/TMC5160_stepper_driver/drv/drv.h
@@ -20,4 +20,7 @@ typedef enum
 //function for setting up driver
 void stp_drv_init(drv_idx_t self, drv_idx_t idx, float i_max/*mA*/, stp_drv_res_t res);
 
+//function for moving motor by given number of steps at given speed and acceleration
+void stp_drv_mov(drv_idx_t self, uint32_t dist/*steps*/, uint16_t v/*steps/s*/, uint16_t a/*steps/s^2*/);
+
 #endif //! __TMC5160_DRV_H__
--- _src/TMC5160_stepper_driver/drv/operation.c
+++ _src/TMC5160_stepper_driver/drv/operation.c
@@ -1,1 +1,6 @@
 #include "drv_const.h"
+
+void stp_drv_mov(drv_idx_t self, uint32_t dist/*steps*/, uint16_t v/*steps/s*/, uint16_t a/*steps/s^2*/)
+{
+
+}
